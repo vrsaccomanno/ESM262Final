1d2a569c6091fe647a184fb4b2a89139a79b44fe
vrsaccomanno <vsaccomanno@bren.ucsb.edu>
2019-06-07 13:05:50 -0700

    progress on second function

--- R/drought_cost.R
+++ R/drought_cost.R
@@ -1,34 +1,32 @@
 #' 
 #' Measure of impact function #1 - 
 #' 
-#' @param clim_data An input dataframe/table populated with data from the SB-LTER meterology station that includes information on daily maximum temperature, minimum temperature, and rainfall from 1942-2016
+#' @param water_use_price An input dataframe/table populated with data from the SB-LTER meterology station that includes information on annual rainfall, average annual water consumption in hectare cubic feet (HCF), and annual average water price in $/HCF from 1942-2016 
 #' 
-#' @format A data frame with 27274 rows and 8 columns
+#' @format A data frame with 75 rows and 4 columns
 #' \itemize {
-#' \item date day/month/year
-#' \item tmin minimum daily temperature (C)
-#' \item tmax maximum daily temperature (C)
-#' \item rain daily rainfall (mm)
 #' \item year
-#' \item month
-#' \item day
-#' \item wy water year
+#' \item mean_rain average annual rainfall (mm)
+#' \item water_consumption average annual household water consumption (HCF)
+#' \item water_price average annual price of water ($/HCF)
 #' }
 #' 
 #' @author Claire Powers and Vienna Saccomanno
-#' 
 #' 
 
 
 compute_water_NPV = function(df, mean_rain, water_consumption, water_price, drought_co = 1.5) {
  
-   if(df$mean_rain <2){
-    drought_cost= df$water_price*drought_co
-    water_bill = df$water_consumption*drought_cost
+  for(i in 1:nrow(mean_rain)){
+    
+   if(df$mean_rain[i] <2){
+    drought_cost = df$water_price[i]*drought_co [i]
+    water_bill = df$water_consumption[i]*drought_cost[i]
   }
   
   else{
-    water_bill = df$water_consumption* df$water_price
+    water_bill = df$water_consumption[i]* df$water_price[i]
+  }
   }
   
   return(list(water_bill=water_bill))
